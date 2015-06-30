d903824b9064167949301944d64aac9d6eb2c638
Adam Rzadkowski <aorzadkowski@gmail.com>
2015-06-29 22:58:31 -0500

    Deleted Server code
    It was ugly anyways

--- ServerThread.java
+++ ServerThread.java
@@ -5,56 +5,5 @@ import java.io.*;
 
 public class ServerThread extends Thread
 {
-    private ServerSocket serverSocket;
-
-    public ServerThread(int port) throws IOException
-    {
-        serverSocket = new ServerSocket(port);
-        serverSocket.setSoTimeout(10000);
-    }
-
-    public void run()
-    {
-        //      while(true)
-        //      {
-        try
-        {
-            System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
-            Socket server = serverSocket.accept();
-            server.setKeepAlive(true);
-            System.out.println("Just connected to " + server.getRemoteSocketAddress());
-            DataInputStream in = new DataInputStream(server.getInputStream());
-            DataOutputStream out = new DataOutputStream(server.getOutputStream());
-            out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress());
-            while (true)
-            {
-                String input = in.readUTF();
-                System.out.println(input);
-                out.writeUTF("SERVER ECHO: " + input);
-                try {sleep(1);} catch (Exception e) {}
-            }
-        }catch(SocketTimeoutException s)
-        {
-            System.out.println("Socket timed out!");
-            //break;
-        }catch(IOException e)
-        {
-            e.printStackTrace();
-            //break;
-        }
-        //      }
-    }
-
-    public static void main(String [] args)
-    {
-        int port = 7777;
-        try
-        {
-            Thread t = new ServerThread(port);
-            t.start();
-        }catch(IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
+    
 }
